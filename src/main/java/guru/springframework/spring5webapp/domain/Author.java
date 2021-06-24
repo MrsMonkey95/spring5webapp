dfd8982099cfec0389e37da5aafefaaa4fd2cfd0
MrsMonkey95 <rahel.engelmann@bwz-rappi.ch>
2021-06-24 09:41:55 +0200

    Added Book.java and Author.java

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.awt.print.Book;
+import java.util.Set;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+}
